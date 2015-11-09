4b047d8124ba2dda1f86befc057790a8a0e0f88c
Hazim Anas <hazimanas93@gmail.com>
2015-11-09 16:28:11 +0800

    Added normalize.css . Added a few new files for actions. Created the rough layout for the app by modifying the css.

---
+++ views/partials/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../assets/css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="../assets/css/style.css" /> 
+	</head>
+	<body>
+		<header>
+			<h1><a href="index.php">Point Tracker</a></h1>
+		</header>
