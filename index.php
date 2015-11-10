6f0fe8edf32a1d51c7a57cfa46803941372103b7
Hazim Anas <hazimanas93@gmail.com>
2015-11-10 23:12:50 +0800

    Added the login form to the index page. Added a redirect to index at login.php actions for the time being

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php include 'views/partials/header.php'; ?>
-	
+<?php include 'views/forms/login.php' ?>
 <?php include 'views/partials/footer.php'; ?>
---
+++ views/forms/login.php
@@ -0,0 +1,6 @@
+<form method="POST" action="actions/login.php">
+	Username : <input type="text" name="username" />
+	Password : <input type="password" name="password" />
+	<button type="submit">Login</button>
+	<button name="register">Register</button>
+</form>
